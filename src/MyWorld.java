e85430f112cbfada2a3ce8928ebb8c4faf1bff40
Aditi Mishra <aditimishra91924@gmail.com>
2024-05-25 14:43:22 +0530

    made another branch

--- src/MyWorld.java
+++ src/MyWorld.java
@@ -1,5 +1,13 @@
 public class MyWorld {
     public static void main(String[] args) {
-
+Human h = new Human( "jack " , 1000);
+    }
+}
+class Human{
+    String name;
+    int salary;
+    Human (String name , int salary){
+        this.name = name ;
+        this.salary = salary;
     }
 }
